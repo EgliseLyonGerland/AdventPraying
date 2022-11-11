d1d0ebe17517836424658c4b6dae937b7014b149
Nicolas Bazille <oltodo@msn.com>
2022-11-11 22:44:21 +0100

    Better display command errors

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import chalk from 'chalk';
 
 import drawCommand from './commands/draw.js';
 import registerCommand from './commands/register.js';
@@ -15,6 +16,9 @@ void yargs(hideBin(process.argv))
   .command(mailCommand)
   .demandCommand()
   .epilog(`© Église Lyon Gerland ${new Date().getFullYear()}`)
-  // .wrap(Math.min(100, yargs))
-  .help()
+  .fail((msg, err, argv) => {
+    console.log(chalk.red(msg));
+    console.log();
+    console.log(argv.help());
+  })
   .parse();
